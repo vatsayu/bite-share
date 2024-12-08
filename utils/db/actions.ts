114f12f652c317c67b922cb423dd1cc1e0974d6f
Ayush Vats <148277599+vatsayu@users.noreply.github.com>
2024-12-08 13:41:02 +0530

    Update actions.ts

--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { db } from './dbConfig';
 import { Users, Reports, Rewards, CollectedWastes, Notifications, Transactions } from './schema';
 import { eq, sql, and, desc,  } from 'drizzle-orm';
